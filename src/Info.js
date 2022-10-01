d5b8d201be5e11649cb82bc9409da2dc930be484
wasim2263 <abdullahwasim42@gmail.com>
2022-10-02 01:54:17 +0600

    added counter

--- src/Info.js
+++ src/Info.js
@@ -1,15 +1,23 @@
+import { useState } from 'react'
+
 const Info = (props) => (
     <div>
       <p> Info: {props.info} </p>
     </div>
 )
 const Hello = (props) => {
+    const [ counter, setCounter ] = useState(0)
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    )
     const currentTime = ()=>{
         return new Date().getFullYear();
     }
-  return (
+
+    return (
       <div>
-        <p>Hello {props.name} {currentTime()}</p>
+        <p>Hello {props.name} {currentTime()} {counter}</p>
       </div>
   )
 }
