05c916dfe35b750415794faff231a7a76df17e25
wasim2263 <abdullahwasim42@gmail.com>
2022-10-17 19:48:12 +0600

    refactored code

--- src/Info.js
+++ src/Info.js
@@ -44,12 +44,14 @@ const Notes = () =>{
     const [showAll, setShowAll] = useState(true)
     const hook = () => {
         console.log('effect')
-        axios
-            .get('http://localhost:3001/notes')
-            .then(response => {
-                console.log('promise fulfilled')
-                setNotes(response.data)
-            })
+
+        const eventHandler = response => {
+            console.log('promise fulfilled')
+            setNotes(response.data)
+        }
+
+        const promise = axios.get('http://localhost:3001/notes')
+        promise.then(eventHandler)
     }
     useEffect(hook, [])
     console.log('render', notes.length, 'notes')
